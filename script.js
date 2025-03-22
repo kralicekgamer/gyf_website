bb8ae16375573fc9e164e67cd7eb2ba52abd7cf9
Matěj Adamec <kralicekgamer@gmail.com>
2025-03-22 21:15:55 +0100

    Add initial HTML, CSS, and JavaScript files for Gyfy project

---
+++ script.js
@@ -0,0 +1,24 @@
+function copyUrl(url) {
+    navigator.clipboard.writeText(url);
+}
+
+const gifPath = "https://gyf.kralyk.dev/gifs/";
+const gifCount = 176;
+let gallery = "";
+
+for (let i = 1; i <= gifCount; i++) {
+    let url = `${gifPath}${i}.gif`;
+    gallery += `
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="${url}" alt="GIF ${i}">
+                <div class="card-body text-center">
+                    <a href="${url}" download class="btn btn-success me-2">Download</a>
+                    <button class="btn btn-primary" onclick="copyUrl('${url}')">Copy me!</button>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+document.write(gallery);
