46cb2489b3220b8acaf09ffc8bb6d9480bd6439a
Matěj Adamec <kralicekgamer@gmail.com>
2025-03-23 22:29:57 +0100

    Fix syntax error in image tag in script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ for (let i = 1; i <= gifCount; i++) {
     gallery += `
         <div class="col-md-4 mb-4">
             <div class="card">
-                <img src="${url}" alt="GIF ${i}" loading="lazy"">
+                <img src="${url}" alt="GIF ${i}" loading="lazy">
                 <div class="card-body text-center">
                     <a href="${url}" download class="btn btn-success me-2">Download</a>
                     <button class="btn btn-primary" onclick="copyUrl('${url}')">Copy me!</button>
